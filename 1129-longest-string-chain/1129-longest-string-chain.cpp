08e88311bfbcac77636229b4197daa10e9e337ed
Shubham Kumar <117706655+Shubham-kumar-iem@users.noreply.github.com>
2023-09-23 15:33:15 +0530

    Time: 123 ms (45.94%), Space: 62 MB (30.77%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        unordered_map<string, int> dp;
+        sort(words.begin(), words.end(), sortBySize);
+        int longestChain = 0;
+
+        for(string word : words) {
+            int longest = 0;
+
+            for(int i=0; i<word.size(); i++) {
+                string sub = word;
+                sub.erase(i, 1);
+                longest = max(longest, dp[sub]+1);
+            }
+            dp[word] = longest;
+            longestChain = max(longestChain, longest);
+        }
+
+        return longestChain;
+    }
+
+    static bool sortBySize(string& a, string& b) {
+        return a.size() < b.size();
+    }
+};
