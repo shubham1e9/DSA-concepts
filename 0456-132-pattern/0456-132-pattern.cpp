4b9d23a6d4c395c6cf4231937ac92348690fbdaf
Shubham Kumar <117706655+Shubham-kumar-iem@users.noreply.github.com>
2023-09-30 17:35:31 +0530

    Time: 55 ms (96.44%), Space: 49 MB (29.11%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        if(nums.size() < 3) {
+            return false;
+        }
+
+        stack<int> st;
+
+        int third = INT_MIN;
+
+        for(int i=nums.size()-1; i>=0; i--) {
+            int curr = nums[i];
+
+            if(curr < third) {
+                return true;
+            }
+
+            while(!st.empty() && st.top() < curr) {
+                third = st.top();
+                st.pop();
+            }
+            st.push(curr);
+        }
+        return false;
+    }
+};
