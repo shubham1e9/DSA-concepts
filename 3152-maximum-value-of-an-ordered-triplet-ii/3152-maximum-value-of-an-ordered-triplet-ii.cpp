d01a60e5e74e36c5992ab0902b7fa2586dc1ca2a
Shubham Kumar <117706655+Shubham-kumar-iem@users.noreply.github.com>
2023-10-01 11:11:45 +0530

    Time: 122 ms (83.33%), Space: 93.7 MB (66.67%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -2,31 +2,25 @@ class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
-        long long ans = 0;
         if(n < 3) return 0;
         
-        // if(n == 3) {
-        //     long long t = (long long)(nums[0] - nums[1]) * nums[2];
-        //     return t > 0 ? t : 0;
-        // }  
-         
-        vector<long long> arr2(n);
-        arr2[n-1] = nums[n-1];
-        
-        for(int i=n-2; i>=0; i--) {
-            arr2[i] = max(arr2[i+1],(long long)nums[i]);
-        }
-        
-        vector<long long> arr(n);
-        arr[0] = nums[0];
+        vector<int> prefix = nums, suffix = nums;
         
         for(int i=1; i<n; i++) {
-            arr[i] = max(arr[i-1],(long long)nums[i]);
+            prefix[i] = max(prefix[i],prefix[i-1]);
         }
-    
+
+        for(int i=n-2; i>=0; i--) {
+            suffix[i] = max(suffix[i], suffix[i+1]);
+        }
+
+        long long ans = 0;
         for(int i=1; i<n-1; i++) {
-            long long temp = (arr[i-1] - (long long)nums[i]) * (long long)arr2[i+1];
-            ans = max(ans, temp);
+            long long vali = prefix[i-1];
+            long long valj = nums[i];
+            long long valk = suffix[i+1];
+            long long val = (vali - valj) * valk;
+            ans = max(ans, val);
         }
         return (ans < 0) ? 0 : ans;
     }
