2f4dcca331baeefdabdbb4debaaa374dfdbe7e78
Shubham Kumar <117706655+Shubham-kumar-iem@users.noreply.github.com>
2023-10-01 20:00:50 +0530

    Time: 52 ms (39.45%), Space: 42 MB (18.18%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -1,18 +1,5 @@
-/**
- * @return {Function}
- */
-// var createHelloWorld = function() {
-//     return function(...args) {
-//         return "Hello World"
-//     }
-// };
-
 let createHelloWorld = () => {
     return () => {
         return "Hello World"
     }
 }
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
