149185747451b2af300153b265a62477f0594ba2
Shubham Kumar <117706655+Shubham-kumar-iem@users.noreply.github.com>
2023-10-04 23:30:37 +0530

    Time: 6 ms (87.59%), Space: 8.7 MB (56.42%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // int n = s.size();
-        // vector<int> v(256, -1);
-        // int right = 0, left = 0, longest = 0;
+        int n = s.size();
+        vector<int> v(256, -1);
+        int right = 0, left = 0, longest = 0;
         
-        // while(right < n) {
-        //     if(v[s[right]] != -1) {
-        //         left = max(left, v[s[right]] + 1);
-        //     }
-        //     v[s[right]] = right;
-        //     longest = max(longest, right - left + 1);
-        //     right++;
-        // }
-        // return longest;
-        int longest = 0;
-
-        for(int i=0; i<s.size(); i++) {
-            unordered_set<char> charset;
-            int curr = 0;
-            for(int j=i; j<s.size(); j++) {
-                if(charset.find(s[j]) == charset.end()) {
-                    charset.insert(s[j]);
-                    curr++;
-                    longest = max(longest, curr);
-                }
-                else {
-                    break;
-                }
+        while(right < n) {
+            if(v[s[right]] != -1) {
+                left = max(left, v[s[right]] + 1);
             }
+            v[s[right]] = right;
+            longest = max(longest, right - left + 1);
+            right++;
         }
         return longest;
+        // int longest = 0;
+
+        // for(int i=0; i<s.size(); i++) {
+        //     unordered_set<char> charset;
+        //     int curr = 0;
+        //     for(int j=i; j<s.size(); j++) {
+        //         if(charset.find(s[j]) == charset.end()) {
+        //             charset.insert(s[j]);
+        //             curr++;
+        //             longest = max(longest, curr);
+        //         }
+        //         else {
+        //             break;
+        //         }
+        //     }
+        // }
+        // return longest;
     }
 };
