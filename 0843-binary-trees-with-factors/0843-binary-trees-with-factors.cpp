c1cb7f53672ae1a7f13c77b18e40e06e2f8b6b91
Shubham Kumar <117706655+Shubham-kumar-iem@users.noreply.github.com>
2023-10-26 10:06:36 +0530

    Time: 21 ms (95.28%), Space: 10.3 MB (33.46%) - LeetHub

---
+++ 0843-binary-trees-with-factors/0843-binary-trees-with-factors.cpp
@@ -0,0 +1,37 @@
+const int MOD = 1e9 + 7;
+
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        unordered_set<int> s(arr.begin(), arr.end());
+        unordered_map<int, int> dp;
+
+        for(int x : arr) {
+            dp[x] = 1;
+        }
+        
+        for(int i : arr) {
+            for(int j : arr) {
+
+                if(j > sqrt(i)) break;
+
+                if(i % j == 0 and s.find(i / j) != s.end()) {
+                    long long temp = (long long)dp[j] * dp[i / j];
+                    if(i / j == j) {
+                        dp[i] = (dp[i] + temp) % MOD;
+                    } 
+                    else {
+                        dp[i] = (dp[i] + temp * 2) % MOD;
+                    }
+                }
+            }
+        }
+        
+        int ans = 0;
+        for (auto& [_, val] : dp) {
+            ans = (ans + val) % MOD;
+        }
+        return ans;
+    }
+};
